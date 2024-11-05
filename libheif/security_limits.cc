9e2d6294ba3e546428963fc9391782f7915bd189
Dirk Lemstra <dirk@lemstra.org>
2024-11-05 19:53:20 +0100

    Patches to make it possible to build with c++17

--- libheif/security_limits.cc
+++ libheif/security_limits.cc
@@ -23,30 +23,30 @@
 
 
 struct heif_security_limits global_security_limits {
-    .version = 1,
+    1,
 
     // --- version 1
 
     // Artificial limit to avoid allocating too much memory.
     // 32768^2 = 1.5 GB as YUV-4:2:0 or 4 GB as RGB32
-    .max_image_size_pixels = 32768 * 32768,
-    .max_number_of_tiles = 4096 * 4096,
-    .max_bayer_pattern_pixels = 16*16,
-    .max_items = 1000,
+    32768 * 32768,
+    4096 * 4096,
+    16*16,
+    1000,
 
-    .max_color_profile_size = 100 * 1024 * 1024, // 100 MB
-    .max_memory_block_size = 512 * 1024 * 1024,  // 512 MB
+    100 * 1024 * 1024, // 100 MB
+    512 * 1024 * 1024,  // 512 MB
 
-    .max_components = 256,
-    .max_iloc_extents_per_item = 32,
-    .max_size_entity_group = 64,
+    256,
+    32,
+    64,
 
-    .max_children_per_box = 100
+    100
 };
 
 
 struct heif_security_limits disabled_security_limits{
-        .version = 1
+        1
 };
 
 
